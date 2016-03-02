9362864d976dd6bbffa031352ab82b3244139709
Paul I <ihnatolia3@gmail.com>
2016-03-02 14:53:25 +0200

    Capistrano setup for deploy

---
+++ config/deploy.rb
@@ -0,0 +1,64 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'disco-kolosseum.de'
+set :repo_url, 'git@github.com:paul-ihnatolia/DiscoApp.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/var/www/disco-kolosseum.de'
+set :deploy_via, :remote_cache
+
+# Default value for :scm is :git
+set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml config/application.yml}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+set :rvm_type, :user
+set :rvm_ruby_version, '2.2.2@disco'
+
+set :passenger_restart_with_touch, true
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  desc "Invoke rake task"
+  task :invoke do
+    on roles(:app) do
+      within "#{current_path}" do
+        with rails_env: :production do
+          execute :rake, ENV['task']
+        end
+      end
+    end
+  end
+end

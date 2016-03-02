550a58bcaba47fde6c94e3e36d854414008a8e4c
Ros Trocyuk <r0st1k.uzh@gmail.com>
2016-03-03 01:21:24 +0200

    keep only last release

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/sys
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-set :keep_releases, 5
+set :keep_releases, 1
 
 set :rvm_type, :user
 set :rvm_ruby_version, '2.2.2@disco'
